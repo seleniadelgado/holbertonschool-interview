cbd0de06f28a3b08a20837735f3f5473ed6a5aad
vagrant <vagrant@vagrant.vm>
2020-03-19 06:59:49 +0000

    made it executable

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
 import fileinput
 
+
 def aggregate_stats():
     try:
         total_size = 0
-        status_code_dict = {}
+        code_dict = {}
         i = 0
         for line in fileinput.input():
             line_list = line.split(' ')
             total_size = total_size + int(line_list[8])
 
             status_code = line_list[7]
-            if status_code in status_code_dict:
-                status_code_dict[status_code] = status_code_dict[status_code] + 1
+            if status_code in code_dict:
+                code_dict[status_code] = code_dict[status_code] + 1
             else:
-                status_code_dict[status_code] = 1
+                code_dict[status_code] = 1
 
             if i % 10 == 9:
                 print('File size: ' + str(total_size))
-                for x, y in status_code_dict.items():
+                for x, y in code_dict.items():
                     print(x + ": " + str(y))
 
             i = i + 1
         except KeyboardInterrupt:
             print('File size: ' + str(total_size))
-            for x, y in status_code_dict.items():
+            for x, y in code_dict.items():
                 print(x + ": " + str(y))
 
-if __name__=='__main__':
+if __name__ == '__main__':
     aggregate_stats()
