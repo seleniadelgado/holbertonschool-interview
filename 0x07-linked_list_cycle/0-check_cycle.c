1ffcd60506e2354630254ea5057960f0afbdf199
vagrant <vagrant@vagrant.vm>
2020-04-02 06:26:53 +0000

    checking for a cycle

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * check_cycle - function checks if a singly linked list has a cycle in it.
+ * @list: parameter for linked list.
+ * Return: 0.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *one = list;
+	listint_t *two = list;
+
+	if (list == NULL)
+		return (0);
+	while (one != NULL && one->next != NULL)
+	{
+		one = one->next->next;
+		two = two->next;
+		if (one == two)
+			return (1);
+	}
+	return (0);
+}
